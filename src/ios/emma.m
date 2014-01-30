799ce8378ff3b3555db769a22d1885c5d5ba3b99
Vyacheslav Shabelnik <vyacheslav@kredito.de>
2014-01-30 13:08:26 +0200

    Order tracking added to ios part.

--- src/ios/emma.m
+++ src/ios/emma.m
@@ -4,7 +4,6 @@
 //
 
 #import <Cordova/CDV.h>
-#import "AppDelegate.h"
 #import "eMMa.h"
 
 @interface emma : CDVPlugin
@@ -18,24 +17,57 @@
     NSString    *callbackId = command.callbackId;
     NSString    *accountID = [command.arguments objectAtIndex:0];
 
-    [eMMa starteMMaSession:accountID];
-
-    [self successWithMessage:[NSString stringWithFormat:@"startSession: accountID = %@;",accountID] toID:callbackId];
+	@try {
+        [eMMa starteMMaSession:accountID];
+		[self success:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"eMMa startSession successful"] callbackId:callbackId];
+    }
+    @catch (NSException *exception) {
+		[self error:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[exception description]] callbackId:callbackId];
+    }
 }
-
--(void)successWithMessage:(NSString *)message toID:(NSString *)callbackID
+- (void) startOrder:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message];
-
-    [self writeJavascript:[commandResult toSuccessCallbackString:callbackID]];
+	NSString    *callbackId = command.callbackId;
+	NSString    *orderID = [command.arguments objectAtIndex:0];
+	NSString    *customerID = [command.arguments objectAtIndex:1];
+	float    totalPrice = [[command.arguments objectAtIndex:2] floatValue];
+		
+	@try {
+		[eMMa startOrder:orderID customerId:customerID totalPrice:totalPrice];
+		[self success:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"eMMa startOrder successful"] callbackId:callbackId];
+	}
+	@catch (NSException *exception) {
+		[self error:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[exception description]] callbackId:callbackId];
+	}
 }
-
--(void)failWithMessage:(NSString *)message toID:(NSString *)callbackID withError:(NSError *)error
+- (void) addProduct:(CDVInvokedUrlCommand*)command
 {
-    NSString        *errorMessage = (error) ? [NSString stringWithFormat:@"%@ - %@", message, [error localizedDescription]] : message;
-    CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
-
-    [self writeJavascript:[commandResult toErrorCallbackString:callbackID]];
+	NSString    *callbackId = command.callbackId;
+	NSString    *productId = [command.arguments objectAtIndex:0];
+	NSString    *name = [command.arguments objectAtIndex:1];
+	float    qty = [[command.arguments objectAtIndex:2] floatValue];
+	float    price = [[command.arguments objectAtIndex:3] floatValue];
+	NSDictionary *extras = [command.arguments objectAtIndex:4];
+			
+	@try {
+		[eMMa addProduct:productId name:name qty:qty price:price extras:extras];
+		[self success:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"eMMa addOrder successful"] callbackId:callbackId];
+	}
+	@catch (NSException *exception) {
+		[self error:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[exception description]] callbackId:callbackId];
+	}
+}
+- (void) trackOrder:(CDVInvokedUrlCommand*)command
+{
+	NSString    *callbackId = command.callbackId;
+	
+	@try {
+		[eMMa trackOrder];
+		[self success:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"eMMa trackOrder successful"] callbackId:callbackId];
+	}
+	@catch (NSException *exception) {
+		[self error:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[exception description]] callbackId:callbackId];
+	}
 }
 
 @end
